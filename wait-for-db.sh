fb47932044e6d3157677e4095b68159f50a8ba86
Osiel Meireles <113800535+Osiel-Meireles@users.noreply.github.com>
2025-10-11 08:30:47 -0300

    Update wait-for-db.sh

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -2,6 +2,10 @@
 set -e
 
 echo "⏳ Aguardando PostgreSQL ficar pronto..."
+# Exporta a senha para que o pg_isready e psql possam usá-la
+export PGPASSWORD=$DB_PASSWORD
+
+# O loop agora usa as variáveis de ambiente corretas
 until pg_isready -h $DB_HOST -U $DB_USER -d postgres; do
   sleep 3
 done
@@ -13,5 +17,8 @@ psql -h $DB_HOST -U $DB_USER -d postgres -tc "SELECT 1 FROM pg_database WHERE da
     psql -h $DB_HOST -U $DB_USER -d postgres -c "CREATE DATABASE \"$DB_NAME\""
 
 echo "✅ Banco $DB_NAME garantido."
+
+# Limpa a senha da variável de ambiente por segurança
+unset PGPASSWORD
 
 exec "$@"
